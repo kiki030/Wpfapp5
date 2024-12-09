0cb843558860a925f2dfe1bf2f1f5ceac8612b38
4b2g0037@office.stust.edu.tw <4b2g0037@office.stust.edu.tw>
2024-12-09 23:51:37 +0800

    更新 UI 元素及增加多個事件處理程序
    更新 `MainWindow.xaml` 標題至 "2024小作家ver2"。
    調整 `applicationToolBar` 寬度至 180。
    增加 `trashButton`、`boldButton`、`italicButton`、`underlineButton` 等按鈕的事件處理程序。
    增加 `fontColorPicker`、`fontFamilyComboBox`、`fontSizeComboBox` 和 `rtbEditor` 的事件處理程序。
    在 `MyDocumentViewer.xaml.cs` 中增加多個引用。
    調整 `fontFamilyComboBox` 和 `fontSizeComboBox` 的預設選項及項目來源格式。
    增加 `OpenCommand_Executed` 和 `SaveCommand_Executed` 方法。

--- MyDocumentViewer.xaml.cs
+++ MyDocumentViewer.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace WpfApp5
@@ -18,9 +21,9 @@ namespace WpfApp5
             {
                 fontFamilyComboBox.Items.Add(fontFamily.Source);
             }
-            fontFamilyComboBox.SelectedIndex = 8;
+            fontFamilyComboBox.SelectedIndex = 13;
 
-            fontSizeComboBox.ItemsSource = new List<double>() { 8,9,10,12,14,16,18,20,22,24,32,40,50,60,80,100 };
+            fontSizeComboBox.ItemsSource = new List<double>() { 8, 9, 10, 12, 14, 16, 18, 20, 22, 24, 32, 40, 50, 60, 80, 100 };
             fontSizeComboBox.SelectedIndex = 3;
         }
 
@@ -32,7 +35,92 @@ namespace WpfApp5
 
         private void OpenCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            openFileDialog.DefaultExt = ".rtf";
+            openFileDialog.AddExtension = true;
 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(openFileDialog.FileName, FileMode.Open);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+                fileStream.Close();
+            }
+        }
+
+        private void SaveCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|HTML File (*.html)|*.html|All files (*.*)|*.*";
+            saveFileDialog.DefaultExt = ".rtf";
+            saveFileDialog.AddExtension = true;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create);
+
+                if (saveFileDialog.FilterIndex == 2) // HTML File
+                {
+                    range.Save(fileStream, DataFormats.Html);
+                }
+                else // Default to RTF
+                {
+                    range.Save(fileStream, DataFormats.Rtf);
+                }
+
+                fileStream.Close();
+            }
+        }
+
+        private void fontColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fontColor = (Color)e.NewValue;
+            SolidColorBrush fontBrush = new SolidColorBrush(fontColor);
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, fontBrush);
+        }
+
+        private void fontSizeComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (fontSizeComboBox.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, fontSizeComboBox.SelectedItem);
+            }
+        }
+
+        private void fontFamilyComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (fontFamilyComboBox.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+            }
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var property_bold = rtbEditor.Selection.GetPropertyValue(TextElement.FontWeightProperty);
+            boldButton.IsChecked = (property_bold != DependencyProperty.UnsetValue) && (property_bold.Equals(FontWeights.Bold));
+
+            var property_italic = rtbEditor.Selection.GetPropertyValue(TextElement.FontStyleProperty);
+            italicButton.IsChecked = (property_italic != DependencyProperty.UnsetValue) && (property_italic.Equals(FontStyles.Italic));
+
+            var property_underline = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            underlineButton.IsChecked = (property_underline != DependencyProperty.UnsetValue) && (property_underline.Equals(TextDecorations.Underline));
+
+            var property_fontFamily = rtbEditor.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+            fontFamilyComboBox.SelectedItem = property_fontFamily.ToString();
+
+            var property_fontSize = rtbEditor.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+            fontSizeComboBox.SelectedItem = property_fontSize;
+
+            var property_fontColor = rtbEditor.Selection.GetPropertyValue(TextElement.ForegroundProperty);
+            fontColorPicker.SelectedColor = ((SolidColorBrush)property_fontColor).Color;
+        }
+
+        private void trashButton_Click(object sender, RoutedEventArgs e)
+        {
+            rtbEditor.Document.Blocks.Clear();
         }
     }
 }
