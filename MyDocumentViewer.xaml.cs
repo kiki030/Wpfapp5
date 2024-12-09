b1b6183f5a93a6cc32f800b50135a5d43fc27631
4b2g0037@office.stust.edu.tw <4b2g0037@office.stust.edu.tw>
2024-12-09 22:58:05 +0800

    新增指令綁定及 UI 元素，優化文件檢視器
    新增了 `Window.CommandBindings` 元素，並在其中添加了三個 `CommandBinding`，分別綁定到 `ApplicationCommands.New`、`ApplicationCommands.Open` 和 `ApplicationCommands.Save` 命令，並指定了對應的執行方法。
    將 `newButton`、`openButton` 和 `saveButton` 的 `Command` 屬性分別設置為 `ApplicationCommands.New`、`ApplicationCommands.Open` 和 `ApplicationCommands.Save`。
    將 `editingToolBar` 的寬度從 400 增加到 450。
    在 `editingToolBar` 中新增了兩個 `ComboBox`，分別用於選擇字體和字體大小。
    在 `DockPanel` 中新增了一個 `RichTextBox`，並設置其名稱為 `rtbEditor`，背景色為 `#FFFFFDE6`，並包含一個空的 `FlowDocument`。
    在 `MyDocumentViewer.xaml.cs` 中，移除了多餘的 `using` 指令，並新增了 `using System.Windows.Media`。
    在 `MyDocumentViewer` 類別中新增了一個 `Color` 類型的 `fontColor` 變數，並在構造函數中初始化 `fontColorPicker` 的 `SelectedColor` 屬性。
    在構造函數中，將系統字體添加到 `fontFamilyComboBox`，並將其預設選項設置為第 8 項。
    在構造函數中，將一組字體大小添加到 `fontSizeComboBox`，並將其預設選項設置為第 3 項。
    新增了 `NewCommand_Executed` 方法，該方法會創建並顯示一個新的 `MyDocumentViewer` 視窗。
    新增了 `OpenCommand_Executed` 方法，目前該方法為空。

--- MyDocumentViewer.xaml.cs
+++ MyDocumentViewer.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfApp5
 {
@@ -19,9 +8,31 @@ namespace WpfApp5
     /// </summary>
     public partial class MyDocumentViewer : Window
     {
+        Color fontColor = Colors.Black;
         public MyDocumentViewer()
         {
             InitializeComponent();
+            fontColorPicker.SelectedColor = fontColor;
+
+            foreach (FontFamily fontFamily in Fonts.SystemFontFamilies)
+            {
+                fontFamilyComboBox.Items.Add(fontFamily.Source);
+            }
+            fontFamilyComboBox.SelectedIndex = 8;
+
+            fontSizeComboBox.ItemsSource = new List<double>() { 8,9,10,12,14,16,18,20,22,24,32,40,50,60,80,100 };
+            fontSizeComboBox.SelectedIndex = 3;
+        }
+
+        private void NewCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MyDocumentViewer myDocumentViewer = new MyDocumentViewer();
+            myDocumentViewer.Show();
+        }
+
+        private void OpenCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+
         }
     }
 }
